7bd0026afaa75c0a5b8f7860cc6d0cacf2c996de
Swidjaja6 <Swidjaja270@gmail.com>
2026-08-27 15:10:52 -0400

    Added Terraform cloud for remote execution, quick nginx.conf note

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,15 @@
 terraform {
   required_version = ">= 1.5"
 
+  # Added for remote state management in Terraform Cloud
+  cloud {
+    organization = "seanslab"
+    workspaces {
+      name = "website-lke"
+    }
+  }
+
+
   required_providers {
     linode = {
       source  = "linode/linode"
